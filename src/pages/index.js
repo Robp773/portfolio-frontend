747559e707e8c350c23e92640bb7b9eb1a941dbd
Robert Peterman <robpeterman573@gmail.com>
2022-04-19 22:23:15 -0400

    added graphql request to test on prod

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,37 @@ import * as React from "react"
 
 // import Layout from "../components/layout"
 // import Seo from "../components/seo"
+import { graphql, Link } from "gatsby"
 
-const IndexPage = () => (
-<div>sadf</div>
-)
+const IndexPage = data => {
+  console.log(data)
+  return <div>index page</div>
+}
 
-
+export const indexPageQuery = graphql`
+  {
+    allStrapiProject {
+      edges {
+        node {
+          title
+          description
+          gallery {
+            localFile {
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
+          }
+          main_image {
+            localFile {
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
 export default IndexPage
