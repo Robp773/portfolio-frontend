44869631ee65c0cf94cbe542557d8ce999eac38d
Robert Peterman <robpeterman573@gmail.com>
2022-04-22 13:41:48 -0400

    WIP on activity page

--- src/pages/activity/index.js
+++ src/pages/activity/index.js
@@ -1,11 +1,54 @@
 import { graphql } from "gatsby"
-import * as React from "react"
+import React, { useEffect, useState } from "react"
 import Layout from "../../components/layout"
+import moment from "moment"
+
+const tableStyles = {
+  width: "50%",
+}
 
 const ActivityPage = data => {
+  const [commits, setCommits] = useState([])
+
+  useEffect(() => {
+    ;(async () => {
+      const response = await fetch(
+        "https://api.github.com/users/robp773/events/public"
+      )
+      const data = await response.json()
+      setCommits(data)
+    })()
+  }, [])
   return (
     <Layout path={data.location.pathname}>
-      activity page
+      <table
+        style={tableStyles}
+        class="bp4-html-table  bp4-html-table-striped bp4-html-table-condensed"
+      >
+        <thead>
+          <tr>
+            <th>Repo</th>
+            <th>Message</th>
+            <th>Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {commits.map(data => {
+            if (data.payload.commits && data.payload.commits[0])
+              return (
+                <tr>
+                  <td>{data.repo.name}</td>
+                  <td>
+                    {data.payload.commits.map(commit => {
+                      return commit.message
+                    })}
+                  </td>
+                  <td>{moment(data.created_at).fromNow()}</td>
+                </tr>
+              )
+          })}
+        </tbody>
+      </table>
     </Layout>
   )
 }
